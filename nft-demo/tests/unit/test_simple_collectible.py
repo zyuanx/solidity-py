671c2759f98d6c018587cee96300ed57a6f9cb47
zyuan <zyuanlee97@gmail.com>
2023-10-17 21:55:33 +0800

    feat

--- nft-demo/tests/unit/test_simple_collectible.py
+++ nft-demo/tests/unit/test_simple_collectible.py
@@ -1,8 +1,11 @@
+from scripts.deploy_and_create import deploy_and_create
 import pytest
 from brownie import accounts, exceptions, network
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 
 
 def test_can_create_simple_collectible():
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for integration testing")
+    simple_collectible = deploy_and_create()
+    assert simple_collectible.ownerOf(0) == get_account()
